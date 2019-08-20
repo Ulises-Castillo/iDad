fb52acb9a4b9d6a3ec588de4b8bf61d82b74b2c5
Ulysses Castillo <ulysses@catalina.io>
2019-08-20 13:13:19 -0700

    Initial Commit

---
+++ iDadTests/iDadTests.swift
@@ -0,0 +1,34 @@
+//
+//  iDadTests.swift
+//  iDadTests
+//
+//  Created by Ulysses Castillo on 8/20/19.
+//  Copyright © 2019 uly. All rights reserved.
+//
+
+import XCTest
+@testable import iDad
+
+class iDadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
