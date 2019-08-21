cbb0c0b0493467bf231aad483f08bfef983080b3
Ulysses Castillo <ulysses@catalina.io>
2019-08-20 17:19:56 -0700

    added custom tableView cell

--- iDad/IDadTableViewController.swift
+++ iDad/IDadTableViewController.swift
@@ -10,44 +10,60 @@ import UIKit
 
 class IDadTableViewController: UITableViewController {
 
-    var iDads:[IDadViewModel] = []
+    var iDadsList:[IDadViewModel] = []
     let iDadListViewModel = IDadListViewModel()
+    
+    // tableview const
+    private let reusableCellID = "IDadTableViewCell"
+    private let profileSegueID = "showProfile"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .red
-        iDads = iDadListViewModel.iDadList //TODO: dynamically observe listViewModel (KVO)
+        iDadsList = iDadListViewModel.iDadList //TODO: dynamically observe listViewModel (KVO)
+        
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 252
+        let nib = UINib(nibName: reusableCellID, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: reusableCellID)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard segue.identifier == "showProfile",
+        guard segue.identifier == profileSegueID,
             let row = tableView.indexPathForSelectedRow?.row,
             let profileVC = segue.destination as? IDadProfileViewController else { return }
         
-            profileVC.iDad = iDads[row]
+            profileVC.iDad = iDadsList[row]
     }
 
     //MARK: table view
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // dequeue cell
-        let reusableCellID = "iDadCell"
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: reusableCellID) else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reusableCellID) as? IDadTableViewCell else {
             fatalError("Unable to dequeue reusable cell with identifier \(reusableCellID)")
         }
         
         // configure cell
-        let iDadViewModel = iDads[indexPath.row]
-        cell.textLabel?.text = iDadViewModel.name
-        cell.detailTextLabel?.text = iDadViewModel.topQuote
-        cell.imageView?.image = iDadViewModel.profilePicture
+        let iDadViewModel = iDadsList[indexPath.row]
+        cell.nameLabel.text = iDadViewModel.name
+        cell.quoteLabel.text = iDadViewModel.topQuote
+        cell.profileImageView.image = iDadViewModel.profilePicture
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return section == 0 ? iDads.count : 0
+        return section == 0 ? iDadsList.count : 0
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: profileSegueID, sender: nil)
     }
 }
 
